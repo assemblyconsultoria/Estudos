ba91027d9f00efd8b813350e3b357e25db3e269e
Assembly Consultoria <assemblyconsultoria@gmail.com>
2022-06-23 23:45:33 -0300

    Realizado ajustes na classe TestaMetodo.java

--- bytebank/src/bytebank/TestaMetodo.java
+++ bytebank/src/bytebank/TestaMetodo.java
@@ -5,14 +5,15 @@ public class TestaMetodo {
 	public static void main(String[] args) {
 		Conta conta = new Conta();
 		
+		conta.nome = "Conta corrente do Silcleiverson";
 		conta.saldo = 100;
-		System.out.println("O saldo da conta é: R$" + conta.saldo);
+		System.out.println("O saldo da conta" + conta.nome + " é: R$" + conta.saldo);
 		
 		conta.deposita(50);
-		System.out.println("Saldo atual agora é: R$" + conta.saldo);
+		System.out.println("Foi recebido um depósito da " + conta.nome + " e o valor agora é: R$" + conta.saldo);
 		
 		conta.saca(70);
-		System.out.println("Saldo atual é: R$" + conta.saldo);
+		System.out.println("Foi realizado um saque de: R$70 reais e o saldo atual é: R$" + conta.saldo);
 
 	}
 
